e9c5dd904196af26b4bda915dd1f0d34e7590a22
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2025-03-23 21:57:08 +0545

    fix: prevent glitches when accessing protected routes

--- src/app/(admin)/admin/(dashboard)/_components/admin-sidebar.tsx
+++ src/app/(admin)/admin/(dashboard)/_components/admin-sidebar.tsx
@@ -56,13 +56,15 @@ const LogoutRow = () => {
   })
 
   return (
-    <button
-      className="flex cursor-pointer items-center gap-x-2 px-4 py-6 duration-300 hover:bg-blue-900"
-      onClick={() => mutate()}
-    >
-      <IoIosLogOut className="text-2xl" />
-      <span> Logout </span>
-    </button>
+    <div className="py-2">
+      <button
+        className="flex w-full cursor-pointer items-center gap-x-2 px-4 py-4 duration-300 hover:bg-blue-900"
+        onClick={() => mutate()}
+      >
+        <IoIosLogOut className="text-2xl" />
+        <span> Logout </span>
+      </button>
+    </div>
   )
 }
 
