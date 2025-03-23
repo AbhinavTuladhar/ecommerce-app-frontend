3baa2b6d1a0fcf41e762bcb702095568bfd8b06e
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2025-03-23 10:30:46 +0545

    feat: implement logout button, use layouts
    - full screen layouts for showing a loading component
    - an admin guard layout for checking whehter the user is authenticated
    and is and admin or not

---
+++ src/layouts/full-screen-layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+/**
+ * Full screen layout, with contents at the centre
+ */
+const FullScreenLayout = ({ children }: { children: React.ReactNode }) => (
+  <div className="grid flex-1 place-items-center">{children}</div>
+)
+
+export default FullScreenLayout
