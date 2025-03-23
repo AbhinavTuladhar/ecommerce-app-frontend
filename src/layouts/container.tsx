88ac4c2fa8526d026d148cb686e60a1253c71fd7
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2025-03-23 21:42:14 +0545

    feat: make layout for sidebar in admin pages

--- src/layouts/container.tsx
+++ src/layouts/container.tsx
@@ -2,7 +2,7 @@ import React, { FC, HTMLAttributes } from 'react'
 import classnames from 'classnames'
 
 const Container: FC<HTMLAttributes<HTMLDivElement>> = ({ children, className, ...props }) => (
-  <div className={classnames('content-grid content-grid--content', className)} {...props}>
+  <div className={classnames('content-grid content-grid--content w-full', className)} {...props}>
     {children}
   </div>
 )
--- src/layouts/full-screen-layout.tsx
+++ src/layouts/full-screen-layout.tsx
@@ -4,7 +4,7 @@ import React from 'react'
  * Full screen layout, with contents at the centre
  */
 const FullScreenLayout = ({ children }: { children: React.ReactNode }) => (
-  <div className="grid flex-1 place-items-center">{children}</div>
+  <div className="grid h-full flex-1 place-items-center">{children}</div>
 )
 
 export default FullScreenLayout
