4c1195a9e5425ce4d138f92abecdc73871e7a47a
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2025-06-28 17:26:49 +0545

    feat: make basic pages for category addition and editing

--- src/app/(admin)/admin/(dashboard)/categories/page.tsx
+++ src/app/(admin)/admin/(dashboard)/categories/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 
 import React from 'react'
+import Link from 'next/link'
+import { GoPlus } from 'react-icons/go'
 
+import Button from '@/components/ui/button'
 import CategoryService from '@/services/category.service'
 import { useQuery } from '@tanstack/react-query'
 
@@ -32,6 +35,16 @@ const CategoryPage = () => {
         <h2 className="text-2xl font-bold"> Category List</h2>
       </div>
       <CategoryTable categoryData={data.data} />
+      <div className="mt-4 flex justify-end">
+        <Link href="/admin/categories/add">
+          <Button variant="dashed">
+            <div className="flex items-center gap-x-1">
+              <GoPlus className="text-xl" />
+              <span className="text-sm">Add category</span>
+            </div>
+          </Button>
+        </Link>
+      </div>
     </>
   )
 }
